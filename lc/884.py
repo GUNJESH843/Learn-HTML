2e7dd922e720acb0bfc4312c9d77dc5fabf2c8e1
GUNJESH KUMAR <gunjesh843@gmail.com>
2024-09-18 22:10:34 +0530

    update

--- lc/884.py
+++ lc/884.py
@@ -2,5 +2,9 @@ class Solution:
     def uncommonFromSentences(self, s1: str, s2: str) -> List[str]:
         sen1 = s1.split(' ')
         sen2 = s2.split(' ')
+        ans = []
         words = sen1 + sen2
-        return [word for word in words if words.count(word) == 1]
+        for word in words: 
+            if words.count(word) == 1:
+                ans.append(word)
+        return ans
