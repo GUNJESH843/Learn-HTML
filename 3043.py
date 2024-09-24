e2bbe8098d59e4c62213f098a3fd5703aa54aefe
GUNJESH KUMAR <gunjesh843@gmail.com>
2024-09-24 22:21:15 +0530

    added missing controller file

--- 3043.py
+++ 3043.py
@@ -1,21 +1,21 @@
-class Solution:
-    def longestCommonPrefix(self, arr1: List[int], arr2: List[int]) -> int:
-        pf = {}
+class Solution: #define the class
+    def longestCommonPrefix(self, arr1: List[int], arr2: List[int]) -> int: #define the function
+        pf = {} #initialize the dictionary
 
-        for i in arr1:
-            str1 = str(i)
-            pre = ""
-            for n in str1:
-                pre += n
-                pf[pre] = pf.get(pre, 0) + 1
-            maxl = 0
+        for i in arr1: #iterate through the first array
+            str1 = str(i)  #convert the integer to string
+            prefix = "" #initialize the prefix
+            for n in str1: #iterate through the string
+                prefix += n #add the character to the prefix
+                pf[prefix] = pf.get(prefix, 0) + 1 #add the prefix to the dictionary
+            maxlength = 0 #initialize the maximum length of the prefix
 
-        for j in arr2:
-            str2 = str(j)
-            pre = ""
-            for m in str2:
-                pre += m
-                if pre in pf:
-                    maxl = max(maxl,len(pre))
-        return maxl
+        for j in arr2: #iterate through the second array
+            str2 = str(j) #convert the integer to string
+            prefix = ""     #initialize the prefix
+            for m in str2: #iterate through the string
+                prefix += m     #add the character to the prefix
+                if prefix in pf: #if the prefix is in the dictionary
+                    maxlength = max(maxlength,len(prefix)) #update the maximum length of the prefix
+        return maxlength #return the maximum length of the prefix
 
